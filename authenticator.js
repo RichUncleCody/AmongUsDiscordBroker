7f403f6955c5cf2561a3b835e11381f1a7cf18eb
Cody Frolander <Cody@Runslow.com>
2020-09-29 17:17:12 -0700

    Cleaned out old junk code

--- authenticator.js
+++ authenticator.js
@@ -5,30 +5,6 @@
     var backend;
 
     authenticator.auth = function (connectcode) {
- /*       console.log(`Client is ${client.connected}`);
-        //Add lookups and errorhandling later
-        client.set(connectcode, process.env.BACKEND_SERVER, function () {
-            console.log("Set connect code on DB");
-            client.expire(connectcode, 60);
-            client.get(connectcode, function (err, result) {
-                console.log("Received connect code from DB");
-                if (backend = result) {
-                    console.log(`Found backend server at: ${process.env.BACKEND_SERVER}`);
-                } else {
-                    backend = "fail";
-                }
-            });
-        });
-        console.log(`Waiting...`);
-        while (!backend) { }
-        if (backend == "fail") {
-            console.log(`background returned: ${background}`)
-            return null;
-        } else {
-            console.log(`background returned: ${background}`)
-            return backend;
-        }*/
-
         client.setAsync(connectcode, process.env.BACKEND_SERVER).then(
             function () {
                 console.log("Set connect code on DB");
@@ -43,7 +19,6 @@
         )
         return backend;
     }
-
     authenticator.authenticated = () => {
         return backend;
     }

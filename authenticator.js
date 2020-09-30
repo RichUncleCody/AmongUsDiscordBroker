7ba00c347b9d44aef34bf5b095571d154a785689
Cody Frolander <Cody@Runslow.com>
2020-09-29 17:22:36 -0700

    Commented on some dumb code

--- authenticator.js
+++ authenticator.js
@@ -5,9 +5,12 @@
     var backend;
 
     authenticator.auth = function (connectcode) {
+        /*Yes I know this code is dumb, it sets the backend and reads it right back...
+         * It's just for testing purposes, once the backends support advertising
+         * through the Redis DB, you can remove the set and expire and it will
+         * auto-connect to whatever backend is designated to service a request*/
         client.setAsync(connectcode, process.env.BACKEND_SERVER).then(
             function () {
-                console.log("Set connect code on DB");
                 client.expire(connectcode, 60);
                 client.getAsync(connectcode).then(
                     function (result) {
